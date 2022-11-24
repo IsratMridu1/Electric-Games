06a6146aaf4c39bcc70057dbcf1a34ebfb24e70d
shams_jubair111 <2017-1-60-032@std.ewubd.edu>
2022-11-24 18:20:16 +0600

    2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import HomePage from "./Pages/HomePage";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
+import GamePage from './Pages/GamePage';
+import HomePage from './Pages/HomePage';
+
 
 function App() {
   return (
     <div>
       <BrowserRouter>
       <Switch>
-        <Route exact path='/'>
-                <HomePage/>
+        <Route exact path='/' >
+                <HomePage/>        </Route>
+        <Route exact path='/games'>
+                <GamePage/>
         </Route>
         <Route exact path='/'>
-                <HomePage/>
-        </Route>
-        <Route exact path='/'>
-                <HomePage/>
+                
         </Route>
       </Switch>
       </BrowserRouter>
